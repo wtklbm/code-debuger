7ce0eed341d5539029de95cb70388027c49509f5
songgenqing <songgenqing@gmail.com>
2021-03-19 19:24:27 +0800

    监控当前文件变更来自动安装语言扩展

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { initCommand } from './core/command';
+import { initEditorListener } from './core/editorListener';
 import { setup } from "./i18n";
 
 export async function activate(context: vscode.ExtensionContext) {
@@ -8,6 +9,9 @@ export async function activate(context: vscode.ExtensionContext) {
 
   // 注册命令
   initCommand(context);
+
+  // 监控文件当前文件
+  initEditorListener();
 }
 
 export function deactivate() {}

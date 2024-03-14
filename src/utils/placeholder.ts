594930a6a21e1c8d0907a7abfe786034b51b5db8
wowbox <sgq10152000@163.com>
2024-03-14 15:15:38 +0800

    修正当前文件作为工作目录问题

--- src/utils/placeholder.ts
+++ src/utils/placeholder.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import * as path from "path";
+import { getVSCodeSetting } from "./api";
 
 
 const PlaceHolders = [
@@ -23,10 +24,11 @@ export function getReplacedString(message: string, uri: vscode.Uri) {
   return result;
 }
 
+// 根据配置来决定是否使用当前文件夹作为工作目录
 export function getWorkspaceFolder(uri: vscode.Uri) {
   if (vscode.workspace.workspaceFolders) {
-    const workspaceFolder = vscode.workspace.getWorkspaceFolder(uri);
-    return workspaceFolder?.uri.fsPath;
+    const fileDirectoryAsCwd = getVSCodeSetting('code-debuger', 'fileDirectoryAsCwd')
+    return fileDirectoryAsCwd ? getFileDirname(uri) : vscode.workspace.getWorkspaceFolder(uri)?.uri.fsPath;
   }
 
   return;
@@ -45,29 +47,34 @@ export function getFile(uri: vscode.Uri) {
   return uri.fsPath;
 }
 
+// 无文件名后缀的文件路径
 export function getFileNoExtension(uri: vscode.Uri) {
   let filepath = getFile(uri);
   let extension = path.extname(filepath);
   return filepath.substring(0, filepath.length - extension.length);
 }
 
+// 相对工作目录路径
 export function getRelativeFile(uri: vscode.Uri) {
   return path.relative(getFile(uri), getWorkspaceFolder(uri) || '');
 }
 
+// 无后缀文件名
 export function getFileBasenameNoExtension(uri: vscode.Uri) {
   let filepath = getFile(uri);
   return path.basename(filepath, path.extname(filepath));
 }
 
+// 文件名
 export function getFileBasename(uri: vscode.Uri) {
   return path.basename(getFile(uri));
 }
 
+// 是文件夹路径
 export function getFileDirname(uri: vscode.Uri) {
   return path.dirname(getFile(uri));
 }
 
 export function getFileExtname(uri: vscode.Uri) {
-  return path.dirname(getFile(uri));
+  return path.extname(getFile(uri));
 }
